9849762c6d5a84cdf2c097ab38f8a5c0850492ed
Genevive Alfirevic <gen@nyble.dev>
2021-12-09 02:02:19 -0600

    Cleanup code

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 async function submit() {
     let username = document.querySelector("input").value;
     let timeframe = document.querySelector("select").value;
+
     console.log(username, timeframe);
+
     let albums = await getAlbums(username, timeframe);
     set_grid(albums);
 }
+
 async function getAlbums(username, timeframe) {
     return await fetch(
         `https://3g5e43is58.execute-api.us-west-2.amazonaws.com/prod/lastfm_import/${username}/${timeframe}`
@@ -14,10 +17,12 @@ async function getAlbums(username, timeframe) {
             return JSON.parse(text.substring(0, text.length - 1) + "]");
         });
 }
+
 function set_grid(albums) {
-    let grid = document.querySelector("div");
+    let grid = document.getElementById("grid")
     grid.innerHTML = "";
-    for (album of albums) {
+
+    for (let album of albums) {
         let image = document.createElement("img");
         image.src = album.src;
         console.log(album.title);
